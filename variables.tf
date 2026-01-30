1cd7711cdabda32dc168497d01f6793fddb05018
Anatolii Vorobiov <anatolii.vorobiov@gmail.com>
2026-01-30 21:47:42 +1300

    Update CIDR validation message and add subnets variable

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "cidr" {
   description = "CIDR range for VPC"
   validation {
     condition     = can(cidrnetmask(var.cidr))
-    error_message = "The vpc_cidr value must be a valid CIDR block (e.g., 10.0.0.0/16)."
+    error_message = "The vpc_cidr value must be a valid CIDR block (e.g., 10.0.0.0/23)."
   }
 }
 
@@ -11,4 +11,13 @@ variable "name" {
   type = string
   description = "VPC name"
   
+}
+
+variable "subnets" {
+  type = map
+  description = "List of subnets in the VPC"
+  default = tomap({
+    int_a = "10.0.0.0/24"
+    int_b = "10.0.1.0/24"
+  })
 }
